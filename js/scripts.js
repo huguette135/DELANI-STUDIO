47eeedfc3fb1a7befdf96630e7d637a1ea954ed9
UMUTONI Huguette <hyguetteumutoni@gmail.com>
2020-10-05 05:59:17 +0200

    addind jquery in js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#design-fig').click(function(){
+        $('#design').toggle();
+        $('#design-icon').toggle();
+    })
+    $('#dev-fig').click(function(){
+        $('#development').toggle();
+        $('#dev-icon').toggle();
