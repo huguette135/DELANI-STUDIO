2a81a7a3ed0394e6590443cdc755701c50bc1b86
UMUTONI Huguette <hyguetteumutoni@gmail.com>
2020-10-05 06:02:35 +0200

    adding toggle in jquery

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,13 @@ $(document).ready(function(){
     $('#dev-fig').click(function(){
         $('#development').toggle();
         $('#dev-icon').toggle();
+    })
+    $('#products-fig').click(function(){
+        $('#product').toggle();
+        $('#products-icon').toggle();
+    })
+    $('#submit').click(function() {
+        let name = $('#name').val();
+        alert(name + ' we have received your message. Thank you for reaching out to us.');
+    })
+})
